9c370a5314d0f78b7bb21402bef431b4435abd8f
Alex Abraham <alexab6104@gmail.com>
2025-05-05 08:05:48 -0400

    Implementation of matlab for visual aid

---
+++ helper.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+plt.ion()
+
+def plot(scores, mean_scores):
+    """
+    Live-plot the training progress:
+      - scores:      list of raw scores per game
+      - mean_scores: list of average score up to each game
+    """
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.ylim(ymin=0)
+    plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show(block=False)
+    plt.pause(0.1)
